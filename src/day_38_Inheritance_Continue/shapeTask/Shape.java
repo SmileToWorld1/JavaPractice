6e35cdeb359afef87411f38899b93ac341f6ed9d
İsmail <ismailsonmez93@gmail.com>
2022-04-11 14:59:12 +0300

    day_38 Shape Task started with Shape Class

---
+++ src/day_38_Inheritance_Continue/shapeTask/Shape.java
@@ -0,0 +1,23 @@
+package day_38_Inheritance_Continue.shapeTask;
+
+public class Shape {
+
+    public String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public double area(){
+        return 0;
+    }
+    public double perimeter(){
+        return 0;
+    }
+    public String toString(){
+        return "Name: "+name+
+                "Area: "+area()+
+                "Perimeter: "+perimeter();
+    }
+
+}
