5733c2acbf2652fc4cccff0e6465f7e8b9064fc5
İsmail <ismailsonmez93@gmail.com>
2022-04-11 15:05:32 +0300

    day_38 Shape Task Circle class created

---
+++ src/day_38_Inheritance_Continue/shapeTask/Circle.java
@@ -0,0 +1,32 @@
+package day_38_Inheritance_Continue.shapeTask;
+
+public class Circle extends Shape{
+
+    public double radius;
+    public static double pi;
+
+    static {
+        pi = 3.14;
+    }
+
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    public double area(){
+        return pi*radius*radius;
+    }
+    public double perimeter(){
+        return 2*pi*radius;
+    }
+
+    public String toString(){
+        return "Name: "+name+
+                "Radius: "+radius+
+                "Pi: "+pi+
+                "Area: "+area()+
+                "Perimeter: "+perimeter();
+    }
+
+}
